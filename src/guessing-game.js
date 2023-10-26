f9073cd01d87ae24d9e5fd3839aae2fb83ffd4ce
Kate Mashko <katika.life@gmail.com>
2023-10-26 13:57:23 +0300

    feat: add solutions

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+this.min =min;
+this.max = max;
     }
 
     guess() {
-
+this.mid = (Math.round((this.min + this.max) / 2));
+return this.mid;
     }
 
     lower() {
-
+  this.max = this.mid;
+  return this.max;
     }
 
     greater() {
-
+this.min = this.mid;
+return this.min;
     }
 }
 
